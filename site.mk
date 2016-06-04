25d0313abd7fb8a7d0ca71ebf386d487c4a72642
Jannis Pinter <jannis@pinterjann.is>
2016-06-04 13:54:20 +0200

    Neue Firmware mit neuen Update-Pfaden und Mesh-Fix

--- site.mk
+++ site.mk
@@ -60,7 +60,7 @@ GLUON_SITE_PACKAGES += \
 	kmod-atl2
 endif
 
-DEFAULT_GLUON_RELEASE := 0.0.1~beta$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.0.2~beta$(shell date '+%Y%m%d')
 
 #Saved for future use
 # $(shell git --git-dir=$(this_dir)/.git \
